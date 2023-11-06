4508e8c8653a21c8ecb0c7c2160f83c851b80a3a
8rando <110241314+8rando@users.noreply.github.com>
2023-11-05 19:35:54 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 /* Add your JavaScript to this file */
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelector('.newsletter form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    var emailInput = document.querySelector('.newsletter input[type="email"]');
+    var messageDiv = document.querySelector('.message');
+    
+    if(emailInput.value === '') {
+      messageDiv.textContent = "Please enter a valid email address.";
+    } else {
+      messageDiv.textContent = "Thank you! Your email address " + emailInput.value + " has been added to our mailing list!";
+    }
+  });
+});

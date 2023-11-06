0b163192d6ec37460e160f5b9f0850b79dd8526e
8rando <110241314+8rando@users.noreply.github.com>
2023-11-05 21:45:54 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,12 +4,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
   document.querySelector('.newsletter form').addEventListener('submit', function(e) {
     e.preventDefault();
     var emailInput = document.querySelector('.newsletter input[type="email"]');
-    var messageDiv = document.querySelector('.message');
     
     if(emailInput.value === '') {
-      messageDiv.textContent = "Please enter a valid email address.";
+      alert("Please enter a valid email address.");
     } else {
-      messageDiv.textContent = "Thank you! Your email address " + emailInput.value + " has been added to our mailing list!";
+      alert("Thank you! Your email address " + emailInput.value + " has been added to our mailing list!");
     }
   });
 });
